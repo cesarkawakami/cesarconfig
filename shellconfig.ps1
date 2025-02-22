0835b9f0f818f4aad88397557b9f4786b4803cb8
Cesar Kawakami <me@crkw.io>
2025-02-22 12:26:33 +0000

    branchless, windows version

--- shellconfig.ps1
+++ shellconfig.ps1
@@ -1,1 +1,7 @@
 Set-PSReadLineOption -EditMode Emacs
+
+function Invoke-GitBranchless {
+    git-branchless wrap -- @args
+}
+Set-Alias -Name g -Value Invoke-GitBranchless
+Set-Alias -Name git -Value Invoke-GitBranchless
